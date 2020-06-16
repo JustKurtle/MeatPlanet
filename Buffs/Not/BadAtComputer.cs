df6f8501c50a915b02bf9f130936d243c7b77805
JustKurtle <curtiskretzer@gmail.com>
2020-06-16 12:41:51 -0500

    BadAtComputer

--- Buffs/Not/BadAtComputer.cs
+++ Buffs/Not/BadAtComputer.cs
@@ -5,26 +5,26 @@ using Terraria.ModLoader;
 
 namespace WorldOfMeat.Buffs.Not
 {
-    public class BadAtComputer : ModBuff
+  public class BadAtComputer : ModBuff
+  {
+    public override void SetDefaults()
     {
-        public override void SetDefaults()
-        {
-            DisplayName.SetDefault("bad at computer. bad at computer!");
-            Description.SetDefault("I AM NOT GOOD WITH COMPUTER");
-            Main.debuff[Type] = true;
-            Main.pvpBuff[Type] = true;
-            Main.buffNoSave[Type] = true;
-            longerExpertDebuff = true;
-        }
-        public override void Update(NPC npc, ref int buffIndex)
-        {
-            npc.defense -= 1;
-            npc.AddBuff(BuffID.Confused, 2, true);
-        }
-        public override void Update(Player player, ref int buffIndex)
-        {
-            player.statDefense -= 1;
-            player.AddBuff(BuffID.Confused, 60, true);
-        }
+      DisplayName.SetDefault("bad at computer. bad at computer!");
+      Description.SetDefault("I AM NOT GOOD WITH COMPUTER");
+      Main.debuff[Type] = true;
+      Main.pvpBuff[Type] = true;
+      Main.buffNoSave[Type] = true;
+      longerExpertDebuff = true;
     }
+    public override void Update(NPC npc, ref int buffIndex)
+    {
+      npc.defense -= 1;
+      npc.AddBuff(BuffID.Confused, 60, true); // upped to 60
+    }
+    public override void Update(Player player, ref int buffIndex)
+    {
+      player.statDefense -= 1;
+      player.AddBuff(BuffID.Confused, 60, true);
+    }
+  }
 }
